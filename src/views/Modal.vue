99de5d879cf001375f314aca1fdef681bb203027
katacoder <pixelslee@foxmail.com>
2024-05-01 22:32:52 +0800

    Provider/inject 测试

---
+++ src/views/Modal.vue
@@ -0,0 +1,36 @@
+<template>
+  <Teleport to="body" :disabled="disabled">
+    <div v-if="value" class="modal">
+      <slot :user="user"></slot>
+      <button @click="$emit('input', false)">Close</button>
+    </div>
+  </Teleport>
+</template>
+
+<script lang="ts" setup>
+import { inject } from 'vue'
+import Teleport from '../components/Teleport.vue'
+import type { UserProfile } from './Test.vue'
+
+defineProps({
+  value: {
+    type: Boolean,
+    default: false,
+  },
+  disabled: {
+    type: Boolean,
+    default: false,
+  },
+})
+const user = inject<UserProfile>('user')
+</script>
+<style>
+.modal {
+  position: fixed;
+  z-index: 999;
+  top: 20%;
+  left: 50%;
+  width: 300px;
+  margin-left: -150px;
+}
+</style>
---
+++ src/views/Profile.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <div>Name: {{ user?.name }}</div>
+  </div>
+</template>
+<script lang="ts" setup>
+defineProps({
+  user: Object,
+})
+</script>
